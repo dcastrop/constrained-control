90dbc4a857173169d7eeeeebb5a3a69076ad1ad6
dcastrop <d.castro-perez@imperial.ac.uk>
2018-04-26 19:48:43 +0100

    Propagated constraints appropriately

--- src/Control/Constrained/Arrow.hs
+++ src/Control/Constrained/Arrow.hs
@@ -10,7 +10,6 @@ import qualified Prelude
 import Prelude hiding ( (.), id, fst, snd)
 
 import Control.Constrained.Category
-import qualified Control.Arrow as Arrow
 
 infixr 3 ***
 infixr 3 &&&
@@ -18,60 +17,68 @@ infixr 2 +++
 infixr 2 |||
 
 class Category t => Arrow t where
+
   arr :: (C t b, C t c) => String -> (b -> c) -> t b c
 
-  fst :: (C t a, C t b)
+  fst :: (C t a, C t (a,b))
       => t (a,b) a
   fst = arr "fst" Prelude.fst
 
-  snd :: (C t a, C t b)
+  snd :: (C t b, C t (a,b))
       => t (a,b) b
   snd = arr "snd" Prelude.snd
 
-  first :: (C t a, C t b, C t c)
+  first :: ( C t c, C t b, C t (a,c) , C t (b,c), C t (c, b) )
         => t a b -> t (a, c) (b, c)
   first = (*** id)
 
-  second :: (C t a, C t b, C t c)
+  second :: ( C t c, C t a, C t b
+           , C t (a,c) , C t (b,c), C t (c, a), C t (c, b) )
          => t a b -> t (c, a) (c, b)
   second = (id ***)
 
-  (***) :: (C t a, C t a', C t b, C t b')
+  (***) :: ( C t b, C t a', C t b', C t (a, b), C t (a', b'), C t (a', b)
+          , C t (b, a') , C t (b', a') )
          => t a a' -> t b b' -> t (a,b) (a',b')
   f *** g = first f >>> arr "swap" swap >>> first g >>> arr "swap" swap
     where swap ~(x,y) = (y,x)
 
-  (&&&) :: (C t a, C t b, C t c)
+  (&&&) :: ( C t a, C t b, C t c, C t (b, a), C t (a, b), C t (c, b)
+          , C t (a, a), C t (b, c) )
         => t a b -> t a c -> t a (b,c)
   f &&& g = arr "dup" (\b -> (b,b)) >>> f *** g
 
 
-instance Arrow.Arrow t => Arrow t where
-  arr _ = Arrow.arr
-  first = Arrow.first
-  second = Arrow.second
-  (***) = (Arrow.***)
-  (&&&) = (Arrow.&&&)
-
 class Arrow a => ArrowChoice a where
 
-  inl :: (C a b, C a c)
+  inl :: ( C a b, C a (Either b c) )
       => a b (Either b c)
   inl = arr "inl" Left
 
-  inr :: (C a b, C a c)
+  inr :: ( C a c, C a (Either b c) )
       => a c (Either b c)
   inr = arr "inr" Right
 
-  left :: (C a b, C a c, C a d)
+  left :: ( C a d, C a c,  C a (Either d b), C a (Either c d)
+         , C a (Either d b), C a (Either b d), C a (Either d c) )
        => a b c -> a (Either b d) (Either c d)
   left = (+++ id)
 
-  right :: (C a b, C a c, C a d)
+  right :: ( C a d, C a b
+          , C a c
+          , C a (Either d b), C a (Either d c)
+          , C a (Either b d), C a (Either c d) )
         => a b c -> a (Either d b) (Either d c)
   right = (id +++)
 
-  (+++) :: (C a b, C a b', C a c, C a c')
+  (+++) :: ( C a b', C a c, C a c'
+          , C a (Either b b')
+          , C a (Either c b')
+          , C a (Either c c')
+          , C a (Either c' c)
+          , C a (Either b' b)
+          , C a (Either b' c)
+          )
         => a b c -> a b' c' -> a (Either b b') (Either c c')
   f +++ g = left f >>> arr "mirror" mirror >>> left g >>> arr "mirror" mirror
     where
@@ -79,15 +86,14 @@ class Arrow a => ArrowChoice a where
       mirror (Left x) = Right x
       mirror (Right y) = Left y
 
-  (|||) :: (C a b, C a d, C a c)
-        => a b d -> a c d -> a (Either b c) d
+  (|||) :: ( C a c, C a d
+          , C a (Either d c)
+          , C a (Either d d)
+          , C a (Either b c)
+          , C a (Either c b)
+          , C a (Either c d) )
+          => a b d -> a c d -> a (Either b c) d
   f ||| g = f +++ g >>> arr "untag" untag
     where
       untag (Left x) = x
       untag (Right y) = y
-
-instance Arrow.ArrowChoice t => ArrowChoice t where
-  left = Arrow.left
-  right = Arrow.right
-  (+++) = (Arrow.+++)
-  (|||) = (Arrow.|||)
--- src/Control/Constrained/Category.hs
+++ src/Control/Constrained/Category.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -13,7 +14,6 @@ module Control.Constrained.Category
 
 import Prelude hiding ( (.), id )
 
-import qualified Control.Category as Category
 import Data.Constraint
 
 
@@ -21,17 +21,10 @@ infixr 9 .
 infixr 1 >>>, <<<
 
 class Category (t :: * -> * -> *) where
-  type C t (a :: *) :: Constraint
+  type C t (a :: *) = (r :: Constraint) | r -> a t
   id :: forall a. C t a => t a a
   (.) :: forall a b c. (C t a, C t b, C t c) => t b c -> t a b -> t a c
 
-class NoConstraint a where
-instance NoConstraint a where
-
-instance Category.Category t => Category t where
-  type C t a = NoConstraint a
-  id  = Category.id
-  (.) = (Category..)
 
 -- | Right-to-left composition
 (<<<) :: (Category cat, C cat a, C cat b, C cat c)
